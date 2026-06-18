813a673e6d19093ad9a29e80ae693d6a74a26002
Valentindjidonou <djidonouvalentin4@gmail.com>
2026-06-18 02:44:13 +0100

    Change API base URL to production endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://skillnova-africa-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
